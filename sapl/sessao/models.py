e5893585b8de589380d53b9fd4d985006785dacb
Leandro Roberto <leandroroberto.br@gmail.com>
2018-10-16 11:00:41 -0300

    retira decorator time_refresh

--- sapl/sessao/models.py
+++ sapl/sessao/models.py
@@ -6,7 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 from model_utils import Choices
 import reversion
 
-from sapl.api.apps import time_refresh
 from sapl.base.models import Autor
 from sapl.materia.models import MateriaLegislativa
 from sapl.parlamentares.models import (CargoMesa, Legislatura, Parlamentar,
@@ -107,7 +106,6 @@ def anexo_upload_path(instance, filename):
     # return get_sessao_media_path(instance, 'anexo', filename)
 
 
-@time_refresh
 @reversion.register()
 class SessaoPlenaria(models.Model):
     # TODO trash??? Seems to have been a FK in the past. Would be:
@@ -267,7 +265,6 @@ class AbstractOrdemDia(models.Model):
             self.numero_ordem, self.materia, self.sessao_plenaria)
 
 
-@time_refresh
 @reversion.register()
 class ExpedienteMateria(AbstractOrdemDia):
 
@@ -380,7 +377,6 @@ class OradorExpediente(AbstractOrador):  # OradoresExpediente
         verbose_name_plural = _('Oradores do Expediente')
 
 
-@time_refresh
 @reversion.register()
 class OrdemDia(AbstractOrdemDia):
 
